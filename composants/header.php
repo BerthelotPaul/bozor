6554fefc6525009cbde8696f2fb9b8fd352a5b69
BerthelotPaul <paulberthelotpro@gmail.com>
2021-11-10 11:22:19 +0100

    first commit

---
+++ composants/header.php
@@ -0,0 +1,8 @@
+<ul>
+    <a href="index.php"><img class="logo" src="logop.png" alt="logo"></a>
+    <li><a href="home.php">Accueil</a></li>
+    <li><a href="nosvaleurs.php">Nos Valeurs</a></li>
+    <li><a href="infos.php">Informations</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="equipe.php">Equipes</a></li>
+</ul>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bozor</title>
+    <link href="../Bozor/css/reset.css" rel="stylesheet">
+    <link href="../Bozor/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include "../Bozor/composants/header.php" ?>
+    <section>
+        <h1 class="home_title">&lt;Bienvenue sur mon site web/&gt;</h1>
+        <h2>J'essaye de faire un site </h2>
+        <p>“J’ai un copain, il est pilote d’essai… enfin, il ne l’est pas encore ; pour l’instant, il essaie d’être pilote !”</p>
+        <p>De Anna de Noailles / Choix de poésies</p>
+        <!--     <iframe width="500" height="200" src="https://www.youtube.com/embed/nVy0JdoLILU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>*
+      -!-->
+
+    </section>
+
+
+
+    <?php include "../Bozor/composants/footer.php" ?>
+</body>
+
+</html>
