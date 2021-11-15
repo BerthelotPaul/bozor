497afbc3f100f1779b865913b42f35a3c1152444
BerthelotPaul <paulberthelotpro@gmail.com>
2021-11-15 18:54:16 +0100

    ajout de bozor2000 et restructuration du projet

---
+++ Bozor2000/bozor2000.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../Bozor2000/bozor2000-style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <video width="320" height="240" autoplay="true" loop>
+        <source src="../Bozor2000/BOZOR2000.mp4">
+    </video>
+    <h1>Choose your destiny</h1>
+    <div class="box-button">
+        <button class="neon">Pierre</button>
+        <button class="neon">Feuilles</button>
+        <button class="neon">Ciseaux</button>
+    </div>
+
+    <div class="resultat"></div>
+
+
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -12,19 +12,7 @@
 
 <body>
     <?php include "../Bozor/composants/header.php" ?>
-    <section>
-        <h1 class="home_title">&lt;Bienvenue sur mon site web/&gt;</h1>
-        <h2>J'essaye de faire un site </h2>
-        <p>“J’ai un copain, il est pilote d’essai… enfin, il ne l’est pas encore ; pour l’instant, il essaie d’être pilote !”</p>
-        <p>De Anna de Noailles / Choix de poésies</p>
-        <!--     <iframe width="500" height="200" src="https://www.youtube.com/embed/nVy0JdoLILU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>*
-      -!-->
-
-    </section>
-
-
-
-    <?php include "../Bozor/composants/footer.php" ?>
+    <p>Allez voir mon tic tac toe juste <a href="../Bozor/Bozor2000/bozor2000.php">ici</a></p>
 </body>
 
 </html>
