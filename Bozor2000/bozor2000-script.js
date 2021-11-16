40076cf255936c2c545d464bb952088523e4a0fd
Paul <paoloberthelot@gmail.com>
2021-11-16 19:01:48 +0100

    function pierre terminer et ca marche !!!

--- Bozor2000/bozor2000-script.js
+++ Bozor2000/bozor2000-script.js
@@ -3,22 +3,32 @@ pierreBouton = document.getElementById("pierre");
 feuilleBouton = document.getElementById("feuille");
 ciseauxBouton = document.getElementById("ciseaux");
 
-armes = ["pierre", "feuille", "ciseaux"];
-ordiChoix = (Math.floor(Math.random() * 3));
 
 
 
+//O = Pierre / 1= Feuille / 2= Ciseaux
 window.pierre = function () {
-    joueur = "pierre";
-    ordi = armes[ordiChoix];
-    affiche.innerHTML = "Le texte qui apparait en JS";
+    ordi = (Math.floor(Math.random() * 3));
+    if (ordi == 0) {
+        affiche.innerHTML = `Joueur : Pierre <br>
+                             Bozor2000 : Pierre <br>
+                             Résultat : Egalité`;
+    } else if (ordi == 1) {
+        affiche.innerHTML = `Joueur : Pierre <br>
+                             Bozor2000 : Feuille <br>
+                             Résultat : Perdu`;
+    } else if (ordi == 2) {
+        affiche.innerHTML = `Joueur : Pierre <br>
+                             Bozor2000 : Ciseaux <br>
+                             Résultat : Gagné`;
+    }
 }
 window.feuille = function () {
     affiche.innerHTML = "C'est un autre texte"
 }
 
 window.ciseaux = function () {
-    affiche.innerHTML = "Autre texte encoreeee"+ordiChoix;
+    affiche.innerHTML = "Autre texte encoreeee" + ordiChoix;
 }
 
 
