8bb58bdec5c07645a2683f38b187776d506f9d28
BerthelotPaul <paulberthelotpro@gmail.com>
2022-03-21 11:34:59 +0100

    icon

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bozor.ico" rel="icon">
     <link href="style.css" rel="stylesheet">
-    <title>Document</title>
+    <title>Bozor</title>
 
 <body>
     <img src="bozor.gif">
